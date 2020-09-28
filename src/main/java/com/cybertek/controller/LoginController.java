aaeff6e9a113217e28164347704293b4ce4a8437
ozzy <ozzy@cybertekschool.com>
2020-09-28 12:30:14 -0400

    Initial Add

--- src/main/java/com/cybertek/controller/LoginController.java
+++ src/main/java/com/cybertek/controller/LoginController.java
@@ -1,18 +1,21 @@
 package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
 	@GetMapping
-	public String login() {
+	public String login(Model model) {
+
 		return "login";
 	}
-	
+
 	@GetMapping("/welcome")
-	public String welcome() {
+	public String welcome(Model model) {
+
 		return "welcome";
 	}
 }
