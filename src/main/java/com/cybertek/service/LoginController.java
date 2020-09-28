be72165c3c18e2da5562bd14fa31f011caeb161e
ozzy <ozzy@cybertekschool.com>
2020-09-28 12:32:04 -0400

    Initial Add

---
+++ src/main/java/com/cybertek/service/LoginController.java
@@ -0,0 +1,21 @@
+package com.cybertek.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping
+	public String login(Model model) {
+
+		return "login";
+	}
+
+	@GetMapping("/welcome")
+	public String welcome(Model model) {
+
+		return "welcome";
+	}
+}
